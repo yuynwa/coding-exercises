41b91420bc0a88a300a55b9be830672e6e23750c
yuynwa <yuynwa@gmail.com>
2018-10-06 00:01:59 +0800

    re-format for better readable

--- lc_reverse_integer.py
+++ lc_reverse_integer.py
@@ -12,29 +12,29 @@
 
     Example 1:
 
-    Input: 123
-    Output: 321
+        Input: 123
+        Output: 321
 
 
     Example 2:
 
-    Input: -123
-    Output: -321
+        Input: -123
+        Output: -321
     
     
     Example 3:
 
-    Input: 120
-    Output: 21
+        Input: 120
+        Output: 21
     
     Note:
-    Assume we are dealing with an environment which could only store 
-    integers within the 32-bit signed integer range: [−231,  231 − 1]. 
-    For the purpose of this problem, assume that your function 
-    returns 0 when the reversed integer overflows.
-
+        Assume we are dealing with an environment which could only store 
+        integers within the 32-bit signed integer range: [−231,  231 − 1]. 
+        For the purpose of this problem, assume that your function 
+        returns 0 when the reversed integer overflows.
 
 '''
+
 
 class Solution:
 
@@ -46,7 +46,6 @@ class Solution:
 
         if not isinstance(x, int):
             raise Exception('%s is not digital !' % x)
-
 
         sign = -1 if x < 0 else 1
         x = sign * x
