07079f9f62ce46679da9fc369aa4e1f230e44104
yuynwa <yuynwa@gmail.com>
2021-01-26 01:23:01 +0800

    :zap: leetcode longest common sebsequence

--- dynamic_programming/lc_longest_common_subsequence.py
+++ dynamic_programming/lc_longest_common_subsequence.py
@@ -56,7 +56,7 @@ def longestCommonSubsequence_mine(text1: str, text2: str) -> int:
             if text1[i] == text2[j]:
                 dp[i+1][j+1] = dp[i][j] + 1
             else:
-                dp[i + 1][j + 1] = max(dp[i][j+1], dp[i+1][j])
+                dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
     return dp[-1][-1]
 
 
