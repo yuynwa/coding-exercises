9e1aaa4bfc1eef38dd06f27c7936125e599a10e7
yuynwa <yuynwa@gmail.com>
2021-01-29 23:56:53 +0800

    :sparkle: leetcode partition list

---
+++ linked_lists/lc_partition_list.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+
+	This problem is from https://leetcode.com
+
+    https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+*/
+
+//type ListNode struct {
+//	Val int
+//	Next *ListNode
+//}
+
+
+func partition(head *ListNode, x int) *ListNode {
+
+	bigger := ListNode{0, nil}
+	biggerN := &bigger
+	curr := head
+
+	smaller := ListNode{0, nil}
+	smallerN := &smaller
+
+	for curr != nil {
+		if curr.Val >= x {
+			biggerN.Next = curr
+			biggerN = curr
+		} else {
+			smallerN.Next = curr
+			smallerN = curr
+		}
+		curr = curr.Next
+	}
+
+	if &biggerN != nil {
+		biggerN.Next = nil
+	}
+
+	if &bigger != nil {
+		smallerN.Next = bigger.Next
+	}
+
+	return smaller.Next
+}
+
+func main() {
+
+	l1 := []int {1,4,3,2,5,2}
+	n1 := NodeList(l1)
+
+	head := partition(n1, 0)
+	PrintNodeList(head)
+}
